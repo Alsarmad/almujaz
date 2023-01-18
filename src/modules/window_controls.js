d83d8676c371b7c69fb60b29a1c3206db445f339
rn0x <ry2n1996@gmail.com>
2023-01-18 11:58:57 +0300

    icon Refresh

--- src/modules/window_controls.js
+++ src/modules/window_controls.js
@@ -3,5 +3,6 @@ module.exports = function window_controls(ipcRenderer) {
     document.getElementById('closed').addEventListener('click', e => ipcRenderer.send('closed'));
     document.getElementById('minimizable').addEventListener('click', e => ipcRenderer.send('minimizable'));
     document.getElementById('minimize').addEventListener('click', e => ipcRenderer.send('minimize'));
+    document.getElementById('Refresh').addEventListener('click', e => document.location.reload());
 
 }
